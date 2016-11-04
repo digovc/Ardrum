811382872d80c9bcb71f9ec8a117c08a5c4bfccb
Rodrigo Daros <rodrigo.conspena@gmail.com>
2016-11-04 15:34:27 -0200

    Adrum:
    - Alterado definições do projeto.

--- Windows/Principal/Properties/AssemblyInfo.cs
+++ Windows/Principal/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ardrum")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Interface para e-Bateria Ardrum.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ardrum")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("DigoSofter")]
+[assembly: AssemblyProduct("Ardrum - Interface para e-Bateria Ardrum.")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
